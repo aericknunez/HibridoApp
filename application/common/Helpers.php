e8678397780512723bf4d01199fce2ce80f141b1
aericknunez <42759691+aericknunez@users.noreply.github.com>
2019-09-13 04:32:11 -0600

    Update Helpers.php

--- application/common/Helpers.php
+++ application/common/Helpers.php
@@ -11,8 +11,8 @@ class Helpers{
           or $_SERVER["SERVER_NAME"] == "www.pizto.com"
           or $_SERVER["SERVER_NAME"] == "superpollo.net"
           or $_SERVER["SERVER_NAME"] == "www.superpollo.net"
-          or $_SERVER["SERVER_NAME"] == "hibridosv.net"
-          or $_SERVER["SERVER_NAME"] == "www.hibridosv.net"){
+          or $_SERVER["SERVER_NAME"] == "hibridosv.com"
+          or $_SERVER["SERVER_NAME"] == "www.hibridosv.com"){
             return TRUE;
           } else {
             return FALSE;
