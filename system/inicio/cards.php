f75f66cc8a2563ec9059a9754000ffc7f83b9c82
Erick Nunez <aerick.nunez@gmail.com>
2020-02-25 19:25:17 -0600

    cards

--- system/inicio/cards.php
+++ system/inicio/cards.php
@@ -10,25 +10,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- division -->
 
 
-<div class="row mt-3">
+<div class="row mt-1">
+
+
 
 <!-- Grid column -->
 <div class="col-xl-3 col-md-6 mb-4">
   <div class="card">
     <div class="row mt-3">
       <div class="col-md-3 col-3 text-left pl-1">
-        <a href="?myempresa" type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="fas fa-barcode" aria-hidden="true"></i></a>
+        <a type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="far fa-chart-bar" aria-hidden="true"></i></a>
       </div>
       <div class="col-md-9 col-9 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->EmpresasAgregadas($_SESSION["username"]); ?></h5>
-        <p class="font-small grey-text">Empresas Agregadas</p>
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo Helpers::Dinero(Helpers::EfectivoPuntos($dash->PuntosProyectados($finicio, $ffin, $_SESSION["username"]))); ?></h5>
+        <p class="font-small grey-text">Comisión Proyectada</p>
       </div>
     </div>
 
   </div>
 </div>
 <!-- Grid column -->
-
 
 
 
@@ -41,14 +42,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <a type="button" class="btn-floating btn-lg success-color ml-4 waves-effect waves-light"><i class="fas fa-chart-line" aria-hidden="true"></i></a>
       </div>
       <div class="col-md-7 col-7 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->TratosCerrados($finicio, $ffin, $_SESSION["username"]); ?></h5>
-        <p class="font-small grey-text">Tratos Cerrados</p>
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo Helpers::Dinero(Helpers::EfectivoPuntos($dash->PuntosObtenidos($finicio, $ffin, $_SESSION["username"]))); ?></h5>
+        <p class="font-small grey-text">Comisión Obtenida</p>
       </div>
     </div>
 
   </div>
 </div>
 <!-- Grid column -->
+
+
+
 
 
 <!-- Grid column -->
@@ -140,22 +144,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Grid column -->
 
 
+
 <!-- Grid column -->
 <div class="col-xl-3 col-md-6 mb-4">
   <div class="card">
     <div class="row mt-3">
       <div class="col-md-3 col-3 text-left pl-1">
-        <a type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="far fa-chart-bar" aria-hidden="true"></i></a>
+        <a href="?myempresa" type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="fas fa-barcode" aria-hidden="true"></i></a>
       </div>
       <div class="col-md-9 col-9 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo Helpers::Dinero(Helpers::EfectivoPuntos($dash->PuntosProyectados($finicio, $ffin, $_SESSION["username"]))); ?></h5>
-        <p class="font-small grey-text">Comisión Proyectada</p>
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->EmpresasAgregadas($_SESSION["username"]); ?></h5>
+        <p class="font-small grey-text">Empresas Agregadas</p>
       </div>
     </div>
 
   </div>
 </div>
 <!-- Grid column -->
+
 
 
 
@@ -168,14 +174,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <a type="button" class="btn-floating btn-lg success-color ml-4 waves-effect waves-light"><i class="fas fa-chart-line" aria-hidden="true"></i></a>
       </div>
       <div class="col-md-7 col-7 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo Helpers::Dinero(Helpers::EfectivoPuntos($dash->PuntosObtenidos($finicio, $ffin, $_SESSION["username"]))); ?></h5>
-        <p class="font-small grey-text">Comisión Obtenida</p>
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->TratosCerrados($finicio, $ffin, $_SESSION["username"]); ?></h5>
+        <p class="font-small grey-text">Tratos Cerrados</p>
       </div>
     </div>
 
   </div>
 </div>
 <!-- Grid column -->
+
 
 
 </div>
--- system/inicio/cards_admin.php
+++ system/inicio/cards_admin.php
@@ -7,101 +7,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-
 <!-- division -->
 
 
-<div class="row mt-3">
-
-<!-- Grid column -->
-<div class="col-xl-3 col-md-6 mb-4">
-  <div class="card">
-    <div class="row mt-3">
-      <div class="col-md-3 col-3 text-left pl-1">
-        <a href="?myempresa" type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="fas fa-barcode" aria-hidden="true"></i></a>
-      </div>
-      <div class="col-md-9 col-9 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->EmpresasAgregadas(); ?></h5>
-        <p class="font-small grey-text">Empresas Agregadas</p>
-      </div>
-    </div>
-
-  </div>
-</div>
-<!-- Grid column -->
-
-
-
-
-
-<!-- Grid column -->
-<div class="col-xl-3 col-md-6 mb-4">
-  <div class="card">
-    <div class="row mt-3">
-      <div class="col-md-5 col-5 text-left pl-1">
-        <a type="button" class="btn-floating btn-lg success-color ml-4 waves-effect waves-light"><i class="fas fa-chart-line" aria-hidden="true"></i></a>
-      </div>
-      <div class="col-md-7 col-7 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->TratosCerrados($finicio, $ffin); ?></h5>
-        <p class="font-small grey-text">Tratos Cerrados</p>
-      </div>
-    </div>
-
-  </div>
-</div>
-<!-- Grid column -->
-
-
-<!-- Grid column -->
-<div class="col-xl-3 col-md-6 mb-4">
-  <div class="card">
-    <div class="row mt-3">
-      <div class="col-md-5 col-5 text-left pl-1">
-        <a type="button" class="btn-floating btn-lg light-blue lighten-1 ml-4 waves-effect waves-light"><i class="fas fa-grin-beam" aria-hidden="true"></i></a>
-      </div>
-      <div class="col-md-7 col-7 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->VisitasPendientes($finicio, $ffin); ?></h5>
-        <p class="font-small grey-text"> Visitas Pendientes </p>
-      </div>
-    </div>
-
-  </div>
-</div>
-<!-- Grid column -->
-
-<!-- Grid column -->
-<div class="col-xl-3 col-md-6 mb-4">
-  <div class="card">
-    <div class="row mt-3">
-      <div class="col-md-5 col-5 text-left pl-1">
-        <a type="button" class="btn-floating btn-lg red accent-2 ml-4 waves-effect waves-light"><i class="fas fa-sliders-h" aria-hidden="true"></i></a>
-      </div>
-      <div class="col-md-7 col-7 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->VisitasRealizadas($finicio, $ffin); ?></h5>
-        <p class="font-small grey-text">Visitas Realizadas</p>
-      </div>
-    </div>
-
-  </div>
-</div>
-<!-- Grid column -->
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-<!-- division -->
-
-
-<div class="row mt-3">
+<div class="row mt-1">
 
 <!-- Grid column -->
 <div class="col-xl-3 col-md-6 mb-4">
@@ -141,22 +50,71 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Grid column -->
 
 
+
 <!-- Grid column -->
 <div class="col-xl-3 col-md-6 mb-4">
   <div class="card">
     <div class="row mt-3">
-      <div class="col-md-3 col-3 text-left pl-1">
-        <a type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="far fa-chart-bar" aria-hidden="true"></i></a>
+      <div class="col-md-5 col-5 text-left pl-1">
+        <a type="button" class="btn-floating btn-lg success-color ml-4 waves-effect waves-light"><i class="fas fa-chart-line" aria-hidden="true"></i></a>
       </div>
-      <div class="col-md-9 col-9 text-right pr-4">
-        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo Helpers::Entero($dash->NoEmpleados()); ?></h5>
-        <p class="font-small grey-text">Cantidad Empleados</p>
+      <div class="col-md-7 col-7 text-right pr-4">
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->TratosCerrados($finicio, $ffin); ?></h5>
+        <p class="font-small grey-text">Tratos Cerrados</p>
       </div>
     </div>
 
   </div>
 </div>
 <!-- Grid column -->
+
+
+<!-- Grid column -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card">
+    <div class="row mt-3">
+      <div class="col-md-5 col-5 text-left pl-1">
+        <a type="button" class="btn-floating btn-lg light-blue lighten-1 ml-4 waves-effect waves-light"><i class="fas fa-grin-beam" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-md-7 col-7 text-right pr-4">
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->VisitasPendientes($finicio, $ffin); ?></h5>
+        <p class="font-small grey-text"> Visitas Pendientes </p>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- Grid column -->
+
+
+
+</div>
+
+
+
+
+<!-- division -->
+
+
+<div class="row mt-3">
+
+<!-- Grid column -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card">
+    <div class="row mt-3">
+      <div class="col-md-3 col-3 text-left pl-1">
+        <a href="?myempresa" type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="fas fa-barcode" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-md-9 col-9 text-right pr-4">
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->EmpresasAgregadas(); ?></h5>
+        <p class="font-small grey-text">Empresas Agregadas</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- Grid column -->
+
 
 
 
@@ -179,4 +137,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Grid column -->
 
 
+
+<!-- Grid column -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card">
+    <div class="row mt-3">
+      <div class="col-md-5 col-5 text-left pl-1">
+        <a type="button" class="btn-floating btn-lg red accent-2 ml-4 waves-effect waves-light"><i class="fas fa-sliders-h" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-md-7 col-7 text-right pr-4">
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo $dash->VisitasRealizadas($finicio, $ffin); ?></h5>
+        <p class="font-small grey-text">Visitas Realizadas</p>
+      </div>
+    </div>
+
+  </div>
 </div>
+<!-- Grid column -->
+
+
+<!-- Grid column -->
+<div class="col-xl-3 col-md-6 mb-4">
+  <div class="card">
+    <div class="row mt-3">
+      <div class="col-md-3 col-3 text-left pl-1">
+        <a type="button" class="btn-floating btn-lg secondary-color ml-4 waves-effect waves-light"><i class="far fa-chart-bar" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-md-9 col-9 text-right pr-4">
+        <h5 class="ml-4 mt-4 mb-2 font-weight-bold"><?php echo Helpers::Entero($dash->NoEmpleados()); ?></h5>
+        <p class="font-small grey-text">Cantidad Empleados</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- Grid column -->
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
