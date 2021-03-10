8fea868c2975eeef8e82e68c5759e91147007dbe
aericknunez <aerick.nunez@gmail.com>
2021-03-09 18:09:24 -0600

    Update variables_db.php

--- application/includes/variables_db.php
+++ application/includes/variables_db.php
@@ -3,10 +3,10 @@ date_default_timezone_set('America/El_Salvador');
 
 if(Helpers::ServerDomain() == TRUE){
 
-define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
-define("USER", "superpol_erick"); 			// The database username. 
-define("PASSWORD", "caca007125-"); 	// The database password.
-define("DATABASE", "superpol_hibridoapp");
+define("HOST", "db5001929171.hosting-data.io"); 			//35.225.56.157 The host you want to connect to. 
+define("USER", "dbu1000577"); 			// The database username. 
+define("PASSWORD", "Caca007125-"); 	// The database password.
+define("DATABASE", "dbs1579005");
   
 
 } else {
@@ -23,8 +23,8 @@ define("DEFAULT_ROLE", "member");
 define("SECURE", FALSE);    // For development purposes only!!!!
 
 // para el sistema
-define("BASE_URL", "https://pizto.com/admin/");
-define("BASEPATH", "https://pizto.com/admin/");	
+define("BASE_URL", "https://app.hibridosv.com/");
+define("BASEPATH", "https://app.hibridosv.com/");	
 define("EMAIL", "aerick.nunez@gmail.com");	
 
 ?>
